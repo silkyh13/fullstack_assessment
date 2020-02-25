38ff32762ef04855de89d392d162304572720c9c
“silkyh13” <silkyh13@gmail.com>
2020-02-24 22:56:17 -0500

    built signup form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,79 @@
 import React, { Component } from "react";
-
+import "../styles/SignUp.css";
 export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
+  }
+
+  handleRegister = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
   render() {
-    return <p>Sign Up</p>;
+    return (
+      <div id="sign-up">
+        <div className="form-container">
+          <div className="form-wrap">
+            <h1>Sign Up</h1>
+            <form>
+              <div className="form-group">
+                <label>First Name </label>
+                <input
+                  type="text"
+                  id="firstName"
+                  onChange={this.handleRegister}
+                ></input>
+              </div>
+
+              <div className="form-group">
+                <label>Last Name </label>
+                <input
+                  type="text"
+                  id="lastName"
+                  onChange={this.handleRegister}
+                ></input>
+              </div>
+
+              <div className="form-group">
+                <label>Email </label>
+                <input
+                  type="email"
+                  id="email"
+                  onChange={this.handleRegister}
+                ></input>
+              </div>
+
+              <div className="form-group">
+                <label>Password </label>
+                <input
+                  type="password"
+                  id="password"
+                  onChange={this.handleRegister}
+                ></input>
+              </div>
+
+              <div className="form-group">
+                <label>Confirm Password</label>
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  onChange={this.handleRegister}
+                ></input>
+              </div>
+              <button>Sign Up</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
