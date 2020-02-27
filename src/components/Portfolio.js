8c460672f1ce1df5ef499a9ae908b5f237b803b4
“silkyh13” <silkyh13@gmail.com>
2020-02-26 21:24:30 -0500

    get account balance of user

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,49 @@
 import React, { Component } from "react";
 import "../styles/Portfolio.css";
 import PortfolioList from "./PortfolioList.js";
+import axios from "axios";
 
 export default class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ticker: "",
+      quantity: 0,
+      balance: 0
+    };
+  }
+  //handle input: ticker's name and quantity
+  handleTransaction = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+  //make post request to purchase stocks
+  handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post("/api/transaction", {
+        ticker: this.state.ticker,
+        quantity: this.state.quantity
+      })
+      .then(transaction => {
+        console.log("success", transaction);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  //get account balance
+  componentDidMount() {
+    axios
+      .get("/api/user")
+      .then(res => {
+        this.setState({
+          balance: res.data.balance
+        });
+      })
+      .catch(err => console.error(err));
+  }
   render() {
     return (
       <div>
@@ -16,15 +57,25 @@ export default class Portfolio extends Component {
           <PortfolioList />
 
           <div className="category">
-            <h3 className="cash">Cash - $5000.00</h3>
+            <h3 className="cash">Cash - ${this.state.balance}</h3>
             <form className="category-form">
               <div className="form-group">
-                <input type="text" placeholder="Ticker"></input>
+                <input
+                  id="ticker"
+                  type="text"
+                  placeholder="Ticker"
+                  onChange={this.handleTransaction}
+                ></input>
               </div>
               <div className="form-group">
-                <input type="text" placeholder="QTY"></input>
+                <input
+                  id="quantity"
+                  type="text"
+                  placeholder="Quantity"
+                  onChange={this.handleTransaction}
+                ></input>
               </div>
-              <button type="submit" className="btn">
+              <button type="submit" className="btn" onClick={this.handleSubmit}>
                 Buy
               </button>
             </form>
